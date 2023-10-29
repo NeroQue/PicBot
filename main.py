98d70c371e8093d845a314114616b64b9eada753
NeroQue <neroque@proton.me>
2023-10-29 16:19:59 +0100

    FEAT: Added support for r34 and started working on support for Gelbooru

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 import aiohttp
-import random  # Import the random module
-from config import DANBOORU_API_KEY, BOT_TOKEN, login
-
+import random
+from config import BOT_TOKEN, login, services
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -13,42 +12,48 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!danbooru'):
-        args = message.content[len('!danbooru '):].strip().split()
+    if message.content.startswith('!danbooru') or message.content.startswith('!gelbooru') or message.content.startswith('!r34'):
+        command, *args = message.content.split()  # Split the message into command and arguments
 
         if not args:
-            await message.channel.send('Please provide a name.')
+            await message.channel.send('Please provide a search query. Usage: !service <query>')
             return
 
-        name = args[0]
-        series = args[1] if len(args) > 1 else name  # Default to name if series is not provided
+        service_name = command[1:]  # Extract the service name (e.g., danbooru, gelbooru, r34)
+        search_query = " ".join(args)  # Combine the remaining arguments as the search query
 
-        # Conditionally construct the search URL based on whether a series is provided
-        if series == name:
-            search_url = f'https://danbooru.donmai.us/posts.json?api_key={DANBOORU_API_KEY}&limit=100&tags={name}&login={login}'
-            print(search_url)
-        else:
-            search_url = f'https://danbooru.donmai.us/posts.json?api_key={DANBOORU_API_KEY}&limit=100&tags={name}_({series})&login={login}'
-            print(search_url)
+        # Check if the service name is valid
+        if service_name in services:
+            # Extract the service-specific base URL
+            service_config = services[service_name]
+            base_url = service_config["base_url"]
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(search_url) as response:
-                print(response)
-                if response.status == 200:
-                    data = await response.json()
-                    if data:
-                        if len(data) == 1:
-                            post = data[0]
+            # Customize the search URL based on the selected service and user input
+            if service_name == "r34":
+                search_url = f'{base_url}?page=dapi&s=post&q=index&json=1&limit=1000&tags={search_query}'
+            else:
+                # For other services, you can add logic to include an API key if required.
+                api_key = service_config.get("api_key", "")
+                search_url = f'{base_url}?api_key={api_key}&limit=100&tags={search_query}&login={login}'
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(search_url) as response:
+                    if response.status == 200:
+                        data = await response.json()
+                        if data:
+                            if len(data) == 1:
+                                post = data[0]
+                            else:
+                                post = random.choice(data)
+
+                            image_url = post['file_url']
+                            await message.channel.send(image_url)
                         else:
-                            # Randomly select a result from the search
-                            post = random.choice(data)
-
-                        image_url = post['file_url']
-                        await message.channel.send(image_url)
+                            await message.channel.send('No results found.')
                     else:
-                        await message.channel.send('No results found.')
-                else:
-                    await message.channel.send('Error: Unable to fetch data from Danbooru.')
+                        await message.channel.send(f'Error: Unable to fetch data from {service_name}.')
+        else:
+            await message.channel.send('Invalid service. Supported services: danbooru, gelbooru, r34')
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 client.run(BOT_TOKEN)
